83d1adb055beb5d857f91e7a2a3fa1a9b377bdd6
Rohan Jain <crodjer@gmail.com>
2016-12-21 15:15:33 +0530

    Solutions to primitive types exercies

--- primitive_types/primitive_types3.rs
+++ primitive_types/primitive_types3.rs
@@ -2,7 +2,8 @@
 // Scroll down for hints!
 
 fn main() {
-    let a = ???
+    // let a = [0; 105];
+    let a = vec![0; 105];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
